caf0fd9d9f44e0fc79594f8a65e3949f3cc61dbc
Alex Varela <alexvar93@gmail.com>
2022-06-16 21:46:23 -0400

    added in headers example

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Header, Param, Post, Put, Res } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 // The @controller decorator tells nest its a controller class
@@ -10,8 +10,16 @@ export class StudentController {
     // "/students"
     @ApiOkResponse({})
     @Get()
-    getStudents(): string {
-        return "All students"
+    @Header("key", "bob")   // Set headers with the @Header decorator
+    // the response's status code is always 200 by default, except for POST requests which use 201. 
+    async getStudents(@Res() res: any): Promise<any> {  //  Example of Async function with await
+        try {
+            const headerExample = await res.getHeaders();
+            console.log(headerExample)
+        }
+        catch(err) {
+            console.log(err)
+        }
     }
 
     // "/students/:id"
@@ -19,8 +27,10 @@ export class StudentController {
     // We need a way to extract the ID to use it
     // We can use @Param() and pass it into the function as an argument 
     // You can specify what part of the params you want. In this case, student Id
-    getStudentById(@Param("studentId") Id: string): string {   // params is an object with studentId property
-        console.log(Id)
+    getStudentById(@Param("studentId") Id: string): string {   // params is an object with studentId property        console.log(res)
+        // res.json({hey: "hello"})    // Nest allows the use of Express's response object. But Nest automatically handles responses on its own by just returning data. It handles it for us.
+        // If you want to use the response object from Express, pass in the @Res decorator
+        
         return `get student with id of ${Id}`
         // Notice you can put at as any variable name you want
         // But inside Param(), you need the exact parameter being extracted (what is in the @Get decorater and route)
@@ -35,7 +45,7 @@ export class StudentController {
     }
 
     @Put("/:studentId")
-    updateStudent(@Param('studentId') studentId: string, @Body() body): string {
+    updateStudent(@Param('studentId') studentId: string, @Body() body, @Res() res: any): string {
         // This function takes the param and body since we need both.
         return `update student with id of ${studentId}, with the following data: ${JSON.stringify(body)}`
     }
