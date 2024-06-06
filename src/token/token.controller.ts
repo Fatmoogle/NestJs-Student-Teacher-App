6ed4da2138f63b7ece4408f1da0011a20f36fd5f
Alexander Varela <alexvar93@gmail.com>
2024-06-06 09:32:08 -0400

    added another change for testing

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,24 +1,34 @@
-import { Body, Controller, Get, Header, HttpCode, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { TokenService } from './token.service';
 
 // The @controller decorator tells nest its a controller class
 // @ApiTags puts everything under it as part of the Students controller
 // for the Swagger docs in the browser
-@ApiTags("Token")
+@ApiTags('Token')
 @Controller('token')
 export class TokenController {
-    constructor(private readonly tokenService: TokenService) {}
+  constructor(private readonly tokenService: TokenService) {}
 
-    @Get()
-    // @Header('Content-Type', 'application/x-www-form-urlencoded')
-    async getToken() {
-        // Have service get token, then send token as reponse through here
-        const token =  await this.tokenService.getToken();
-        return token
-    }
+  @Get()
+  // @Header('Content-Type', 'application/x-www-form-urlencoded')
+  async getToken() {
+    // Have service get token, then send token as reponse through here
+    const token = await this.tokenService.getToken();
+    return token;
+  }
 
-    // TEST
-    // more TEST
-    // even more test
-} 
+  // TEST
+  // more TEST
+  // even more test
+  // even more test AGAIN
+}
